09c62543215ed159fd14777ed0624b9eafbd785f
fourjr <28086837+fourjr@users.noreply.github.com>
2020-05-11 19:12:51 +0800

    Examples

--- script.js
+++ script.js
@@ -1,99 +1,155 @@
+examples = [
+    [
+`1000 (Group 1)\t1100 (Group 2)\t1200 (Group 3)
+Tom\tWilliam\tOliver
+Liam\tJames\tHarry
+\tElijah\t`,
+`Hi {name},
 
-        $('textarea').keydown(function (e) {
-            var keyCode = e.keyCode || e.which;
+We would like to meet you tomorrow, 2 March at {time}.
 
-            if (keyCode === $.ui.keyCode.TAB) {
-                e.preventDefault();
+See you!`
+    ],
+    [
+`11 May, 12 pm (Group 1)\t12 May, 10 am (Group 2)\t12 May, 3 pm (Group 3)
+Emma\tMia\tAmelia
+\t\tHarper
+\t\tAmy`,
+`Dear {name},
 
-                // The one-liner that does the magic
-                document.execCommand('insertText', false, "\t");
+Please note the following details for your interview:
+Date: *{date}*
+Time: *{time}*
+Location: Room B
+
+Best regards`
+    ]
+]
+
+$('textarea').keydown(function (e) {
+    var keyCode = e.keyCode || e.which
+
+    if (keyCode === $.ui.keyCode.TAB) {
+        e.preventDefault()
+
+        // The one-liner that does the magic
+        document.execCommand('insertText', false, '\t')
+    }
+})
+
+String.prototype.toTitleCase = function() {
+    // Converts 'apple pie' to 'Apple Pie'
+    return this.replace(
+        /\w*/g,
+        txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    )
+}
+
+function inputChange() {
+    let val = $('#input-value').val()
+    let messageFMT = $('#message-fmt').val()
+    let table = $('#input-table')
+
+    if (!val || !messageFMT) {
+        $('.inputTable').css('display', 'none')
+        $('.textAreaSingle').css('display', 'none')
+        return
+    }
+
+    let splitVal = val.split('\n')
+
+    table.html('')
+    $('.inputTable').css('display', 'block')
+    $('.textAreaSingle').css('display', 'block')
+
+    let header = true
+    for (i of splitVal) {
+        let row = $('<tr>')                
+        for (j of i.split('\t')) {
+            let col
+            if (header) {
+                col = $('<th>')
             }
-        });
-
-        String.prototype.toTitleCase = function() {
-            // Converts 'apple pie' to 'Apple Pie'
-            return this.replace(
-                /\w*/g,
-                txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-            );
-        };
-
-        function inputChange() {
-            let val = $('#input-value').val()
-            let messageFMT = $('#message-fmt').val()
-            let table = $('#input-table')
-
-            if (!val || !messageFMT) {
-                $('.inputTable').css('display', 'none');
-                $('.textAreaSingle').css('display', 'none')
-                return
+            else {
+                col = $('<td>')
             }
-
-            let splitVal = val.split('\n')
-
-            table.html('')
-            $('.inputTable').css('display', 'block');
-            $('.textAreaSingle').css('display', 'block')
-
-            let header = true;
-            for (i of splitVal) {
-                let row = $('<tr>')                
-                for (j of i.split('\t')) {
-                    let col;
-                    if (header) {
-                        col = $('<th>')
-                    }
-                    else {
-                        col = $('<td>')
-                    }
-                    col.text(j)
-                    row.append(col)
-                }
-                table.append(row)
-                header = false;
-            }
-
-            let users = [];
-            let linenum = 0;
-            for (line of splitVal) {
-                if (linenum > 0) {
-                    let usernum = 0;
-                    for (user of line.split("\t")) {
-                        if (linenum == 1) {
-                            users.push([user.split("(")[0].trim()])
-                        }
-                        else {
-                            users[usernum].push(user.split("(")[0].trim())
-                        }
-                    }
-                }
-                linenum += 1
-            }
-
-            let messages = []
-            let groupno = 0;
-            for (group of users) {
-                for (name of group) {
-                    if (name) {
-                        messages.push(messageFMT.replace(/{name}/g, name.toTitleCase()).replace(/{time}/g, val.split("\n")[0].split("\t")[groupno].split(" ")[0]))
-                    }
-                }
-            }
-
-            result = $('#result')
-            result.val('')
-            result.val(messages.join("\n----------------------\n"))
+            col.text(j)
+            row.append(col)
         }
+        table.append(row)
+        header = false
+    }
 
-        function copy() {
-            $('#result').select()
-            document.execCommand('copy')
+    let users = []
+    let linenum = 0
+    for (line of splitVal) {
+        if (linenum > 0) {
+            let usernum = 0
+            for (user of line.split('\t')) {
+                if (linenum == 1) {
+                    users.push([user.split('(')[0].trim()])
+                }
+                else {
+                    users[usernum].push(user.split('(')[0].trim())
+                }
+            }
         }
+        linenum += 1
+    }
 
-        function reset() {
-            $('#result').val('')
-            $('#input-value').val('')
-            $('#message-fmt').val('')
-            $('.inputTable').css('display', 'none');
-            $('.textAreaSingle').css('display', 'none')
+    let messages = []
+    let groupno = 0
+    for (group of users) {
+        for (name of group) {
+            if (name) {
+                let header = val.split('\n')[0].split('\t')[groupno].split('(')[0].trim()
+                let timeDate = header.split(',')
+                let time, date
+                if (timeDate.length == 1) {
+                    time = timeDate[0].trim()
+                    date = '{date}'
+                }
+                else {
+                    time = timeDate[timeDate.length - 1].trim()
+                    date = timeDate.slice(0, timeDate.length - 1).join(',').trim()
+                }
+                messages.push(
+                    messageFMT.replace(/{name}/g, name.toTitleCase())
+                              .replace(/{time}/g, time)
+                              .replace(/{date}/g, date)
+                )
+            }
         }
+        groupno += 1
+    }
+
+    result = $('#result')
+    result.val('')
+    result.val(messages.join('\n----------------------\n'))
+}
+
+
+function showTooltip(type) {
+
+}
+function hideTooltip(type) {
+    
+}
+function example(index) {
+    $('#input-value').val(examples[index][0])
+    $('#message-fmt').val(examples[index][1])
+    inputChange()
+}
+
+function copy() {
+    $('#result').select()
+    document.execCommand('copy')
+}
+
+function reset() {
+    $('#result').val('')
+    $('#input-value').val('')
+    $('#message-fmt').val('')
+    $('.inputTable').css('display', 'none')
+    $('.textAreaSingle').css('display', 'none')
+}
